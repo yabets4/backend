5377c5a5a9935bd9141eca4ece1d17a45b792fba
yabets4 <90160641+yabets4@users.noreply.github.com>
2025-09-23 16:22:27 +0300

    Update db.loader.js

--- src/loaders/db.loader.js
+++ src/loaders/db.loader.js
@@ -10,6 +10,11 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false, // required for many managed cloud DBs like Render
+  },
 });
 
-export default pool;   // ✅ actual Pool instance
+export default pool;
+  // ✅ actual Pool instance
+
